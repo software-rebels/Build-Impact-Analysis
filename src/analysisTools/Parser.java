3997a463e00b22325b27cf6f0963856aef57b2a5
Shane McIntosh <shanemcintosh@acm.org>
2016-07-17 12:21:33 -0400

    Clean up the parser a little bit

--- src/analysisTools/Parser.java
+++ src/analysisTools/Parser.java
@@ -5,48 +5,71 @@ import java.io.*;
 
 public class Parser
 {
-	private String log;
+	private String inFile;
+
+  private final int TYPE_IDX = 0;
+  private final String TYPE_COMMIT = "commit";
+  private final String TYPE_DATE = "Date:";
+  private final String TYPE_MODIFIED = "M";
+  private final String TYPE_ADDED = "A";
+
+  private final int CID_IDX = 1;
+  private final int CID_LENGTH = 40;
+
+  private final String DELIM = ",";
 		
-	public Parser(String log)
+	public Parser(String inFile)
 	{
-		this.log = log;
+		this.inFile= inFile;
 	}
 	
 	public void parseLog(String destinationFile, int noOfCommitsToParse) throws IOException
 	{
-		BufferedReader br = new BufferedReader(new FileReader(new File(log)));
+		BufferedReader br = new BufferedReader(new FileReader(new File(inFile)));
 		BufferedWriter bw = new BufferedWriter(new FileWriter(new File(destinationFile)));
-		String readLine = "";
-		int i =0;
+		String readLine = br.readLine();
 		
-		while(i<noOfCommitsToParse+1)
+		for(int i = 0; i<noOfCommitsToParse+1; readLine = br.readLine())
 		{
-			readLine = br.readLine();
-			String[] temp=readLine.split(" ");
-			if(temp.length>0 && temp[0].equals("commit"))
+			String[] lineSplit=readLine.split(" ");
+
+      // Skip the lines that cannot be split
+      if(lineSplit.length <= 0)
+      {
+        continue;
+      }
+
+      switch(lineSplit[TYPE_IDX])
 			{
-				if(temp[1].length()==40)
-				{
-					if(i!=0)
-					{
-						bw.newLine();
-					}
-					bw.write(readLine+",");
-					i++;
-				}
-			}
-			else if(temp.length>0 && temp[0].equals("Date:"))
-			{
-				bw.write(readLine+",");
-			}
-			else if(temp.length>0 && (temp[0].contains("M") || temp[0].contains("A")))
-			{
-				if(temp.length==1)
-				{
-					bw.write(temp[0]+",");
-				}
-			}
-		}
+        case TYPE_COMMIT: // Handle the start of a commit
+				  if(lineSplit[CID_IDX].length()==CID_LENGTH)
+				  {
+					  if(i!=0)
+					  {
+						  bw.newLine();
+					  }
+					  bw.write(readLine+DELIM);
+					  i++;
+				  }
+          break;
+
+        case TYPE_DATE: // Handle the date part of a commit
+          bw.write(readLine+DELIM);
+          break;
+
+        case TYPE_MODIFIED: // Handle the files in a commit
+        case TYPE_ADDED:
+				  if(lineSplit.length==1)
+				  {
+					  bw.write(lineSplit[TYPE_IDX]+DELIM);
+				  }
+          break;
+
+        default:
+          // TODO: Is there anything that needs to be done here?
+          break;
+		  }
+    }
 		
 		br.close();
 		bw.close();
